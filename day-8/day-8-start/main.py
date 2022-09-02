ac2dd6a1033032672f7cb95fba39c3374710d876
atilla kalay <atilla.kalayy@gmail.com>
2022-09-02 23:35:15 +0300

    Day 8 continuing.

---
+++ day-8/day-8-start/main.py
@@ -0,0 +1,17 @@
+# Review:
+# Create a function called greet().
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+#def greet(name):
+#    print(f"Hello {name}")
+#   print(f"How do you do {name}")
+#  print(f"Isn't the weather nice today {name}?")
+
+#greet("Atilla")
+
+def greet_with(name,location):
+    print(f"Hello {name}")
+    print(f"What is the like in {location}")
+
+greet_with(name="Atilla",location="Istanbul")
